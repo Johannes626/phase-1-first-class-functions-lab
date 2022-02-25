6db0da3265e6a45d4afb4c3f01627abb2f5f2db4
Johannes Vargas <johannes.vargas1@gmail.com>
2022-02-25 11:27:06 -0600

    commit tree 703be772ea8330f3b7302aa5e5bb87e20e03d646

--- index.js
+++ index.js
@@ -1,1 +1,31 @@
-// Code your solution in this file!
+const returnFirstTwoDrivers = () => {
+    return ['Antonia', 'Nuru']
+}
+
+const returnLastTwoDrivers = () => {
+    return ['Amari','Mo']
+}
+
+const selectingDrivers = [returnFirstTwoDrivers, returnLastTwoDrivers]
+
+const createFareMultiplier = (int) => {
+    return (fare) => {
+        return int*fare
+    }
+}
+
+const fareDoubler = (doubleFare) => {
+    if (typeof doubleFare === 'number')
+    return createFareMultiplier(2)(doubleFare)
+}
+
+const fareTripler = (tripleFare) => {
+    if (typeof tripleFare === 'number')
+    return createFareMultiplier(3)(tripleFare)
+}
+
+const currentDrivers = ['Johannes', 'Onelis']
+
+const selectDifferentDrivers = (currentDrivers, callback) => {
+    return currentDrivers.slice(0,2), callback()
+}
